fe7fd4a8bf6d1e00bdaf279300b12eb2d673fa8c
Alexander Shvets <neochief@shvetsgroup.com>
2017-02-21 19:27:36 +0200

    Moved pure Birman layout out of this repo. Added apostrophe on tilde key.

--- Ubuntu/install.sh
+++ Ubuntu/install.sh
@@ -5,16 +5,24 @@ if [ $(id -u) -ne 0 ]
   exit
 fi
 
+sudo rm /usr/share/X11/xkb/symbols/typo-uk
+sudo rm /usr/share/X11/xkb/symbols/typo-ru-ukrainian
 sudo cp symbols/* /usr/share/X11/xkb/symbols/
 
-# /usr/share/X11/xkb/rules/evdev.lst
+# Edit /usr/share/X11/xkb/rules/evdev.lst
 
-sudo sed -i -E 's/\s*typo(-ru|-uk|_ukrainian|_russian).*//g' /usr/share/X11/xkb/rules/evdev.lst
-sudo sed -i -E 's/(! layout)/\1\n  typo-ru         Russian (Typograhic)\n  typo-uk         Ukrainian (Typographic)/g' /usr/share/X11/xkb/rules/evdev.lst
-sudo sed -i -E 's/(! variant)/\1\n  typo_ukrainian  typo-ru: Russian (Typographic + Ukrainian)\n  typo_russian    typo-uk: Ukrainian (Typographic + Russian)/g' /usr/share/X11/xkb/rules/evdev.lst
+sudo sed -i -E 's/\s*typo(-ru|-uk).*//g' /usr/share/X11/xkb/rules/evdev.lst
+sudo sed -i -E 's/(! layout)/\1\n  typo-uk         Ukrainian (Typographic)\n  typo-ru-ukrainian         Russian (Typographic + Ukrainian)/g' /usr/share/X11/xkb/rules/evdev.lst
+sudo sed -i -E 's/(! variant)/\1\n  typo-uk-russian    typo-uk: Ukrainian (Typographic + Russian)/g' /usr/share/X11/xkb/rules/evdev.lst
 
-# /usr/share/X11/xkb/rules/evdev.xml
+# Edit /usr/share/X11/xkb/rules/evdev.xml
 
-sudo awk '/<\/layoutList>/ { system ( "cat ./rules/envdev.xml" ) } { print; }' ~/Desktop/evdev.xml >> /tmp/evdev.xml
+sudo awk '/<\/layoutList>/ { system ( "cat ./rules/envdev.xml" ) } { print; }' /usr/share/X11/xkb/rules/evdev.xml >> /tmp/evdev.xml
 sudo rm /usr/share/X11/xkb/rules/evdev.xml
 sudo mv /tmp/evdev.xml /usr/share/X11/xkb/rules/evdev.xml
+
+# Enable AltGr
+gsettings set org.gnome.desktop.input-sources xkb-options "['lv3:ralt_switch']"
+
+# Show further instructions
+echo "Please log out and log in again, and then enable keyboard layouts at System Settings > Text Entry (search for \"Typographic\" input sources)."
